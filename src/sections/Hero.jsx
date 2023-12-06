b7a2937b494b91e32479140b32348101635c0085
Enes Ceylan <63819815+MedusaCollins@users.noreply.github.com>
2023-12-06 09:17:44 +0300

    Refactor image import and update profile picture styling.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import myPic from '../public/image/ProfilePic.jpg'
+import {profilePic} from '../import.js'
 import { motion } from 'framer-motion'
 
 const Hero = () => {
@@ -10,7 +10,7 @@ const Hero = () => {
         initial={{ opacity: 0, y: -300, zIndex:20}}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.5 }}>
-      <img src={myPic} alt="myPic" className=' w-32 sm:w-56 sm:absolute mx-auto left-0 right-0 sm:mx-0 sm:left-7 sm:right-0 z-20'/>
+      <img src={profilePic} alt="myPic" className=' w-32 sm:w-56 sm:absolute mx-auto left-0 right-0 sm:mx-0 sm:left-7 sm:right-0 z-20 border'/>
       </motion.div>
       <motion.div 
         className='right-0 sm:absolute'
@@ -18,7 +18,7 @@ const Hero = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.5 }}>
       <h1 className='text-3xl sm:text-5xl text-center sm:text-left font-medium tracking-widest mt-2 mr-1'>
-        Merhaba! ben <br/><b className='text-4xl sm:text-6xl font-bold'>Enes Ceylan</b></h1>
+        Hi! I am<br/><b className='text-4xl sm:text-6xl font-bold'>Enes Ceylan</b></h1>
       </motion.div>
       <motion.div 
         className=' bg-[#16E0BD] sm:absolute bottom-5 w-full sm:text-right text-center p-2 z-10 text-lg sm:text-2xl'
